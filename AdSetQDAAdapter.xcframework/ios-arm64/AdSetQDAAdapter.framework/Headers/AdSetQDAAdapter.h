50d3889060c00316c76a105a6a6c23efa48615bf
yaohaofei <313888178@qq.com>
2026-01-16 15:01:29 +0800

    feat: release v1.0.0

---
+++ AdSetQDAAdapter.xcframework/ios-arm64/AdSetQDAAdapter.framework/Headers/AdSetQDAAdapter.h
@@ -0,0 +1,16 @@
+//
+//  AdSetQDAAdapter.h
+//  AdSetQDAAdapter
+//
+//  Created by Shens on 8/1/2026.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdSetQDAAdapter.
+FOUNDATION_EXPORT double AdSetQDAAdapterVersionNumber;
+
+//! Project version string for AdSetQDAAdapter.
+FOUNDATION_EXPORT const unsigned char AdSetQDAAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdSetQDAAdapter/PublicHeader.h>
